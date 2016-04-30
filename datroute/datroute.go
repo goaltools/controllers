7cf14a1fc2d1269d1569ba95b34eadbae14c50a3
alconaut <alconautgh@yandex.com>
2016-04-30 19:40:29 +0600

    Added a controller for parsing input parameters

---
+++ datroute/datroute.go
@@ -0,0 +1,60 @@
+// Package datroute parses user sent data and combines
+// it with the parameters that are passed by the
+// [datroute](https://github.com/goaltools/datroute) router.
+package datroute
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var (
+	// maxMem is a number of MB per upload that is stored in memory.
+	// The remainder is stored on disk in temporary files.
+	maxMem = flag.Int64("requests:max.memory", 32, "number of MB to store in memory when parsing a file")
+)
+
+// DATRoute is a controller that does two things:
+// 1. Calls Request.ParseForm to parse GET / POST requests;
+// 2. Makes Request available in your controller (use c.Request).
+type DATRoute struct {
+	Request *http.Request `bind:"request"`
+}
+
+// Before calls ParseForm of the c.Request. Moreover,
+// parameters extracted from URN by the datroute router
+// are saved to the Form field of the Request.
+func (c *DATRoute) Before() http.Handler {
+	// Save the old value of Form, datroute router
+	// uses it to pass parameters extracted from URN.
+	t := c.Request.Form
+
+	// Set r.Form to nil, otherwise ParseForm / ParseMultipartForm will not work.
+	c.Request.Form = nil
+
+	// Parse the body depending on the Content-Type.
+	var err error
+	switch c.Request.Header.Get("Content-Type") {
+	case "multipart/form-data":
+		err = c.Request.ParseMultipartForm(*maxMem << 20)
+	default:
+		err = c.Request.ParseForm()
+	}
+
+	// Make sure the parsing was successfull.
+	// Otherwise, return a "bad request" error.
+	if err != nil {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		})
+	}
+
+	// Add the old values from router to the new r.Form.
+	// Copying only one value per key as the router does not pass more than that.
+	for k := range t {
+		c.Request.Form.Add(k, t.Get(k))
+	}
+	return nil
+}
