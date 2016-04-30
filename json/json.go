5aa732945b1f8a8960f9e1fd960c11fab2d42606
alconaut <alconautgh@yandex.com>
2016-04-30 16:42:25 +0600

    Updated json controller's actions

--- json/json.go
+++ json/json.go
@@ -4,7 +4,9 @@
 package json
 
 import (
+	"encoding/json"
 	"flag"
+	"log"
 	"net/http"
 )
 
@@ -20,8 +22,36 @@ var (
 type JSON struct {
 }
 
-// RenderJSON gets any object as input and returns a type
-// that implements http.Handler interface.
-func (c *JSON) RenderJSON(obj interface{}) http.Handler {
-	return Handler{obj, http.StatusOK}
+// RenderJSON gets any object and a status code to use
+// as input and returns a handler function that can be used
+// for rendering of the result.
+func (c *JSON) RenderJSON(obj interface{}, status int) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var b []byte
+		var err error
+
+		// Marshal the object either in a human readable format or as is.
+		switch *indent {
+		case true:
+			b, err = json.MarshalIndent(obj, *indPrefix, *indString)
+		default:
+			b, err = json.Marshal(obj)
+		}
+
+		// Make sure there are no any errors.
+		if err != nil {
+			go log.Println(err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+
+		// Write the the result to the response.
+		w.WriteHeader(status)
+		w.Write(b)
+	})
+}
+
+// RenderJSONOK is an equivalent of calling RenderJSON(obj, http.StatusOK).
+func (c *JSON) RenderJSONOK(obj interface{}) http.Handler {
+	return c.RenderJSON(obj, http.StatusOK)
 }
